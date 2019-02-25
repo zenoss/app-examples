7057381bba83edb07751fd97946b6388282d3d87
Chet Luther <chet.luther@gmail.com>
2019-02-25 16:27:28 -0500

    python-statsd example
    Working example of a Python Flask app using statsd (statsd-telegraf) to
    publish metrics.
    
    Also added "requester" as part of this to generate a request against
    each example app once every two seconds.

---
+++ python-statsd/python_statsd.py
@@ -0,0 +1,42 @@
+# stdlib Imports
+import os
+import sys
+
+# Third Party Imports
+from flask import Flask
+from statsd import StatsClient
+
+
+# Read configuration from the environment.
+STATSD_HOST = os.environ.get("STATSD_HOST", None)
+STATSD_PORT = os.environ.get("STATSD_PORT", 8125)
+
+
+# Define the key:value tags we want to send with all metrics.
+METRIC_TAGS = {
+    "contextUUID": "example.python.statsd",
+    "meta_type": "com.example.python.statsd",
+    "name": "Python StatsD Example",
+}
+
+
+# Configure reporter to send metrics to statsd.
+if STATSD_HOST:
+    STATSD = StatsClient(STATSD_HOST, STATSD_PORT)
+else:
+    sys.exit("STATSD_HOST must be set")
+
+
+# Create the Flask app.
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    # Increment a counter metric.
+    STATSD.incr("root.requests", tags=METRIC_TAGS)
+    return "Hello! (python-statsd)"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
---
+++ python-statsd/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="python-statsd",
+    version="1.0",
+    description="Example of sending metrics using statsd.",
+    url="https://github.com/cluther/app-examples",
+    author="Chet Luther",
+    author_email="chet.luther@gmail.com",
+    license="MIT",
+    py_modules=["python_statsd"],
+    zip_safe=False)
