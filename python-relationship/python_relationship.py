84afca4d681e0ee37760e87bbc415939ce6e3c0b
Chet Luther <chet.luther@gmail.com>
2019-02-26 14:59:47 -0500

    add python-relationship example
    An example of using indexed tags to supply the custom relationship
    building tags: simpleCustomRelationship(Source|Sink)Tag. This will
    result in seeing the "app-examples.gostatsd" entity as related to the
    "Python Relationship Example" entity in Smart View.
    
    Refs ZPS-5287.

---
+++ python-relationship/python_relationship.py
@@ -0,0 +1,49 @@
+# stdlib Imports
+import os
+import sys
+
+# Third Party Imports
+from flask import Flask
+from statsd import StatsClient
+
+
+# Read configuration from the environment.
+STATSD_HOST = os.environ.get("STATSD_HOST")
+STATSD_PORT = os.environ.get("STATSD_PORT", 8125)
+
+
+# Define the key:value tags we want to send with all metrics.
+METRIC_TAGS = {
+    "contextUUID": "example.python.relationship",
+    "meta_type": "com.example.python.relationship",
+    "name": "Python Relationship Example",
+}
+
+# Configure reporter to send metrics to statsd.
+if STATSD_HOST:
+    STATSD = StatsClient(STATSD_HOST, STATSD_PORT)
+else:
+    sys.exit("STATSD_HOST must be set")
+
+
+# Create the Flask app.
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    # Increment a counter metric.
+    STATSD.incr("root.requests", tags=METRIC_TAGS)
+    return "Hello! (python-relationship)"
+
+
+def set_relationship_tags():
+    """Add relationship tags from environment."""
+    sink_tags = os.environ.get("RELATIONSHIP_SINK_TAGS", "")
+    for i, tag in enumerate(sink_tags.strip().split()):
+        METRIC_TAGS[f'simpleCustomRelationshipSinkTag.{i}'] = tag
+
+
+if __name__ == "__main__":
+    set_relationship_tags()
+    app.run(host="0.0.0.0")
---
+++ python-relationship/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="python-relationship",
+    version="1.0",
+    description="Example of providing relationships.",
+    url="https://github.com/cluther/app-examples",
+    author="Chet Luther",
+    author_email="chet.luther@gmail.com",
+    license="MIT",
+    py_modules=["python_relationship"],
+    zip_safe=False)
