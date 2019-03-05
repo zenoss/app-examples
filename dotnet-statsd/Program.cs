f6e9266553a7a6da8eeadf4952fa981e6b6186dd
Chet Luther <chet.luther@gmail.com>
2019-03-05 17:23:10 -0500

    add dotnet-statsd example
    Equivalent to python-statsd example, but in .NET Core.
    
    Refs ZPS-5287.

---
+++ dotnet-statsd/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+using JustEat.StatsD;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace dotnet_statsd
+{
+    static class Metrics
+    {
+        private static Dictionary<string, string> MetricTags = new Dictionary<string, string>() {
+            ["contextUUID"] = "example.dotnet.statsd",
+            ["meta_type"] = "com.example.dotnet.statsd",
+            ["name"] = ".NET StatsD Example",
+        };
+
+        /// Name returns basename with MetricTags appended.
+        public static string Name(string basename) =>
+            String.Format("{0},{1}", basename, String.Join(",",
+                MetricTags.Select(
+                    x => string.Format("{0}={1}", x.Key, x.Value))));
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateWebHostBuilder(args).Build().Run();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(serviceCollection =>
+                    serviceCollection.AddStatsD(
+                        Environment.GetEnvironmentVariable("STATSD_HOST")))
+                .UseUrls("http://0.0.0.0:5000")
+                .UseStartup<Startup>();
+    }
+
+    public class Startup
+    {
+        private IStatsDPublisher stats;
+
+        public Startup(IStatsDPublisher statsDPublisher)
+        {
+            stats = statsDPublisher;
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.Run(async (context) =>
+            {
+                stats.Increment(Metrics.Name("root.requests"));
+                await context.Response.WriteAsync("Hello! (dotnet-statsd)");
+            });
+        }
+    }
+}
